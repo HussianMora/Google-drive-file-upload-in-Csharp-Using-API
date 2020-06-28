5863470e04132d13db8c06b25e2154d46e3d43ec
Hussain Morawala <hmorawala@gmail.com>
2020-06-28 17:25:03 +0530

    Add project files.

---
+++ assignment_googleapidrivev3/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace assignment_googleapidrivev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Upload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(478, 92);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 20);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(304, 130);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(152, 28);
+            this.Upload.TabIndex = 3;
+            this.Upload.Text = "Upload";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Upload File on google drive";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(586, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Note:Upload .pdf,.jpeg,.txt files only";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(256, 180);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 120);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Upload);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Upload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ assignment_googleapidrivev3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+namespace assignment_googleapidrivev3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Authorize()
+        {
+            string[] scopes = new string[] { DriveService.Scope.Drive,
+                               DriveService.Scope.DriveFile,};
+            var clientId = "446138855943-edvr5bskptd3bivfui61tkb13nfdepu7.apps.googleusercontent.com"; 
+            var clientSecret = "4RZCxGSDmFaAL7arBj4jSpUB";            
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            }, scopes,
+            Environment.UserName, CancellationToken.None, new FileDataStore("MyAppsToken")).Result;
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "MyAppName",
+
+            });
+            service.HttpClient.Timeout = TimeSpan.FromMinutes(100);
+            textBox2.Visible = true;
+            var respocne = uploadFile(service, textBox1.Text, "");
+            MessageBox.Show("Process completed--- Response--" + respocne);
+            textBox1.Text = "";
+        }
+        public Google.Apis.Drive.v3.Data.File uploadFile(DriveService _service, string _uploadFile, string _parent, string _descrp = "Uploaded with .NET!")
+        {
+            if (System.IO.File.Exists(_uploadFile))
+            {
+                Google.Apis.Drive.v3.Data.File body = new Google.Apis.Drive.v3.Data.File();
+                body.Name = System.IO.Path.GetFileName(_uploadFile);
+                body.Description = _descrp;
+                body.MimeType = GetMimeType(_uploadFile);
+                byte[] byteArray = System.IO.File.ReadAllBytes(_uploadFile);
+                System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+                try
+                {
+                    FilesResource.CreateMediaUpload request = _service.Files.Create(body, stream, GetMimeType(_uploadFile));
+                    request.SupportsTeamDrives = true;
+                    request.ProgressChanged += Request_ProgressChanged;
+                    request.ResponseReceived += Request_ResponseReceived;
+                    request.Upload();
+                    return request.ResponseBody;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Error Occured");
+                    return null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The file does not exist.");
+                return null;
+            }
+        }
+        private string GetMimeType(string uploadFile)
+        {
+            throw new NotImplementedException();
+        }
+        private void Request_ProgressChanged(Google.Apis.Upload.IUploadProgress obj)
+        {
+            textBox2.Text += obj.Status + " " + obj.BytesSent;
+        }
+        private void Request_ResponseReceived(Google.Apis.Drive.v3.Data.File obj)
+        {
+            if (obj != null)
+            {
+                MessageBox.Show("File was uploaded sucessfully--" + obj.Id);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+           
+        }
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.ToString()=="")
+            {
+
+                MessageBox.Show("No Files Selected");
+            }
+            else
+            {
+                Authorize();
+            }
+            
+        }
+
+    }
+}
